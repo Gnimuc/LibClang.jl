dd02f1ecf8c27ddc385fc3e5bd9e7db3cdfa00b2
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2021-05-05 07:26:54 +0000

    build based on 9284840

---
+++ v0.60.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#","page":"Home","title":"Home","text":"CurrentModule = LibClang","category":"page"},{"location":"#LibClang-1","page":"Home","title":"LibClang","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [LibClang]","category":"page"}]
+}
