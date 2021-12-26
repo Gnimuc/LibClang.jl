8562e911272989d019f2a16ef5f59cad2a8e4f36
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2021-12-26 06:45:10 +0000

    build based on 59c840e

---
+++ v0.61.1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#","page":"Home","title":"Home","text":"CurrentModule = LibClang","category":"page"},{"location":"#API-Reference-1","page":"Home","title":"API Reference","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"Modules = [LibClang]\nOrder = [:constant, :function, :type]","category":"page"}]
+}
--- versions.js
+++ versions.js
@@ -1,5 +1,6 @@
 var DOC_VERSIONS = [
   "stable",
+  "v0.61",
   "v0.60",
   "dev",
 ];
